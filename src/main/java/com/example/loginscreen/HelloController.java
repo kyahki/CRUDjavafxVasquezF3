eef33517fae32760c58e5bde22570a43f064c471
kyahki <genjikyle123@gmail.com>
2024-04-21 13:35:27 +0800

    yeyo

--- src/main/java/com/example/loginscreen/HelloController.java
+++ src/main/java/com/example/loginscreen/HelloController.java
@@ -1,11 +1,14 @@
 package com.example.loginscreen;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -14,67 +17,186 @@ import javafx.scene.paint.Color;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    public Pane pnLogout,pnGoBack;
-    public VBox pnLogin, pnRegister;
+    public static User user;
+    public Pane pnLogout,pnGoBack,pnRead;
+    public VBox pnLogin, pnRegister,pnUpdate;
 
     public ColorPicker cpPicker;
 
-    MenuItem menuItem1 = new MenuItem("DarkMode");
-    MenuItem menuItem2 = new MenuItem("LightMode");
+    @FXML
+    private  TableView<User> userTableView;
 
 
-    private static final String[] validUsernames = {"user1", "user2", "user3"};
-    private static final String[] validPasswords = {"pass1", "pass2", "pass3"};
+
     @FXML
     private TextField txtUser;
     @FXML
     private PasswordField txtPass;
     @FXML
-    private TextField txtRegisterUser;
+    private TextField txtRegisterUser,txtNewUsername;
     @FXML
-    private PasswordField txtRegisterPass;
+    private PasswordField txtRegisterPass,txtNewPassword;
+
+
     @FXML
-    protected void onHelloButtonClick() throws IOException {
+    private void onHelloButtonClick() throws IOException {
         String username = txtUser.getText();
         String password = txtPass.getText();
         boolean isValidCredentials = false;
         AnchorPane p = (AnchorPane) pnLogin.getParent();
-        for (int i = 0; i < validUsernames.length; i++) {
-            if (username.equals(validUsernames[i]) && password.equals(validPasswords[i])) {
-                isValidCredentials = true;
-                break;
+
+        int retrievedUserID = 0;
+        try (Connection c = MySqlConnection.getConnection();
+             Statement statement = c.createStatement()) {
+            String query = "SELECT * FROM users";
+            ResultSet res = statement.executeQuery(query);
+//
+            while (res.next()) {
+                String username2 = res.getString("name");
+                String password2 = res.getString("password");
+
+                if (username.equals(username2) && password.equals(password2)) {
+                    isValidCredentials = true;
+                    retrievedUserID = res.getInt("id");
+                    user = new User(retrievedUserID,username2,password2);
+                    break;
+                }
             }
-            else{
-                p.getScene().getStylesheets().clear();
-                p.getScene().getStylesheets().add(getClass().getResource("user1.css").toExternalForm());
-                Parent scene = FXMLLoader.load(getClass().getResource("incorrect.fxml"));
-                scene.prefHeight(p.getScene().getHeight());
-                scene.prefWidth(p.getScene().getWidth());
-                p.getChildren().clear();
-                p.getChildren().add(scene);
-            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
         if (isValidCredentials) {
-//            AnchorPane p = (AnchorPane) pnLogin.getParent();
+            p = (AnchorPane) pnLogin.getParent();
             p.getScene().getStylesheets().clear();
             p.getScene().getStylesheets().add(getClass().getResource("user1.css").toExternalForm());
-            Parent scene = FXMLLoader.load(getClass().getResource("home-view.fxml"));
+            Parent scene = FXMLLoader.load(getClass().getResource("updateAccount.fxml"));
+            scene.prefHeight(p.getScene().getHeight());
+            scene.prefWidth(p.getScene().getWidth());
+            p.getChildren().clear();
+            p.getChildren().add(scene);
+        } else {
+            p.getScene().getStylesheets().clear();
+            p.getScene().getStylesheets().add(getClass().getResource("user1.css").toExternalForm());
+            Parent scene = FXMLLoader.load(getClass().getResource("incorrect.fxml"));
             scene.prefHeight(p.getScene().getHeight());
             scene.prefWidth(p.getScene().getWidth());
             p.getChildren().clear();
             p.getChildren().add(scene);
         }
+
     }
+//    @FXML
+//    private void onReadAccount() throws IOException {
+//        try (Connection c = MySqlConnection.getConnection();
+//             Statement statement = c.createStatement()) {
+//            String query = "SELECT * FROM users";
+//            ResultSet res = statement.executeQuery(query);
+//
+//            // Clear existing items in the TableView
+//            userTableView.getItems().clear();
+//
+//            while (res.next()) {
+//                int id = res.getInt("id");
+//                String name = res.getString("name");
+//                String password = res.getString("password");
+//
+//                // Create a new row with the retrieved data
+//                User user = new User(id, name, password);
+//
+//                // Add the row to the TableView
+//                userTableView.getItems().add(user);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+@FXML
+private void onReadAccount() throws IOException {
+    AnchorPane p = (AnchorPane) pnUpdate.getParent();
+    Parent scene = FXMLLoader.load(getClass().getResource("readAcc.fxml"));
+    p.getChildren().clear();
+    p.getChildren().add(scene);
+
+    try (Connection c = MySqlConnection.getConnection();
+         Statement statement = c.createStatement()) {
+        String query = "SELECT * FROM users";
+        ResultSet res = statement.executeQuery(query);
+
+
+        List<User> userList = new ArrayList<>();
+
+        while (res.next()) {
+            int id = res.getInt("id");
+            String name = res.getString("name");
+            String password = res.getString("password");
+
+
+            userList.add(new User(id, name, password));
+        }
+
+        TableView<User> userTableView = new TableView<>();
+
+        TableColumn<User, Integer> idColumn = new TableColumn<>("ID");
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        idColumn.setPrefWidth(100);
+
+        TableColumn<User, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        nameColumn.setPrefWidth(200);
+
+        TableColumn<User, String> passwordColumn = new TableColumn<>("Password");
+        passwordColumn.setCellValueFactory(new PropertyValueFactory<>("password"));
+        passwordColumn.setPrefWidth(200);
+
+        userTableView.getColumns().addAll(idColumn, nameColumn, passwordColumn);
+        userTableView.setItems(FXCollections.observableArrayList(userList));
+
+
+        userTableView.setPrefWidth(500);
+
+        p.getChildren().add(userTableView);
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+}
+
+
+
+
+
+//    private void populateUserListFromDatabase() {
+//        userList.clear(); // Clear existing data
+//
+//        try (Connection c = MySqlConnection.getConnection();
+//             Statement statement = c.createStatement()) {
+//            String query = "SELECT * FROM users";
+//            ResultSet res = statement.executeQuery(query);
+//
+//            while (res.next()) {
+//                int id = res.getInt("id");
+//                String username = res.getString("name");
+//                String password = res.getString("password");
+//                userList.add(new User(id, username, password));
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+
+
     @FXML
-    protected void onRegisterClick() throws  IOException{
+    private void onRegisterClick() throws  IOException{
         AnchorPane p = (AnchorPane) pnLogin.getParent();
         p.getScene().getStylesheets().clear();
         p.getScene().getStylesheets().add(getClass().getResource("user1.css").toExternalForm());
@@ -84,6 +206,39 @@ public class HelloController {
         p.getChildren().clear();
         p.getChildren().add(scene);
     }
+
+    @FXML
+    private void onUpdateUsername() {
+        int currentUserId = user.getId();
+        String newUsername = txtNewUsername.getText();
+        UpdateData updateData = new UpdateData();
+        try {
+            updateData.updateUsername(currentUserId, newUsername);
+            System.out.println("New username set successfully!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+        @FXML
+        protected void onUpdatePassword() {
+            int currentUserId = user.getId();
+            String newPassword = txtNewPassword.getText();
+
+            UpdateData updateData = new UpdateData();
+            try {
+                updateData.updatePassword(currentUserId, newPassword);
+                System.out.println("New password set successfully!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+
 
 
 
@@ -104,8 +259,17 @@ public class HelloController {
 //
 //        }
 //    }
+            // ^^^^^^^^^^^^^^^^^^
+            //    p = (AnchorPane) pnLogin.getParent();
+            //            p.getScene().getStylesheets().clear();
+            //            p.getScene().getStylesheets().add(getClass().getResource("user1.css").toExternalForm());
+            //    Parent scene = FXMLLoader.load(getClass().getResource("home-view.fxml"));
+            //            scene.prefHeight(p.getScene().getHeight());
+            //            scene.prefWidth(p.getScene().getWidth());
+            //            p.getChildren().clear();
+            //            p.getChildren().add(scene);
     @FXML
-    protected  void onGoBack(ActionEvent actionEvent) throws IOException{
+    private void onGoBack(ActionEvent actionEvent) throws IOException{
         AnchorPane p = (AnchorPane) pnGoBack.getParent();
         Parent scene = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
         p.getChildren().clear();
@@ -113,7 +277,7 @@ public class HelloController {
 
     }
     @FXML
-    protected  void onLogout(ActionEvent actionEvent) throws IOException{
+    private  void onLogout(ActionEvent actionEvent) throws IOException{
         Color color = cpPicker.getValue();
 
        String realColor = "rgb(" +
@@ -136,10 +300,10 @@ public class HelloController {
     }
 
     @FXML
-    protected void onCreateAccount(){
+    private void onCreateAccount() throws IOException {
         String username = txtRegisterUser.getText();
         String password = txtRegisterPass.getText();
-        AnchorPane p = (AnchorPane) pnRegister.getParent();
+        AnchorPane p;
         try(Connection c = MySqlConnection.getConnection();
             PreparedStatement statement = c.prepareStatement(
                     "INSERT into users (name, password) VALUES (?,?)")){
@@ -154,6 +318,37 @@ public class HelloController {
         }catch (SQLException e){
             throw new RuntimeException(e);
         }
+        p = (AnchorPane) pnRegister.getParent();
+        Parent scene = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        p.getChildren().clear();
+        p.getChildren().add(scene);
+    }
+
+    @FXML
+    private void onDeleteAccount() throws IOException {
+        AnchorPane p = (AnchorPane) pnUpdate.getParent();
+        try(Connection c = MySqlConnection.getConnection();
+        PreparedStatement statement = c.prepareStatement(
+                "DELETE FROM users WHERE id=? RETURNING *"
+        )){
+            int id = user.getId();
+            statement.setInt(1,id);
+            int rows = statement.executeUpdate();
+            ResultSet res = statement.getResultSet();
+            if(res.next()){
+                System.out.println("User successfully deleted!");
+                System.out.println("Name: " + res.getString("name"));
+                System.out.println("Password: "+ res.getString("password"));
+            }
+            System.out.println("Rows Deleted: " + rows);
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        Parent scene = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        p.getChildren().clear();
+        p.getChildren().add(scene);
     }
 
 }
--- src/main/java/com/example/loginscreen/UpdateData.java
+++ src/main/java/com/example/loginscreen/UpdateData.java
@@ -5,23 +5,34 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class UpdateData {
-    public static void main(String[] args) {
-        try(Connection c = MySqlConnection.getConnection();
-            PreparedStatement statement = c.prepareStatement(
-                    "UPDATE users SET name=? WHERE id=?"
-            )){
-            String name = "Tisumi";
-            int id = 1;
-            statement.setInt(2, id);
-            statement.setString(1, name);
-            int rows = statement.executeUpdate();
-            if(rows > 0){
-                System.out.println("Rows updated: "+ rows);
+
+    public void updateUsername(int userIdToUpdate, String newUsername) throws SQLException {
+        try (Connection connection = MySqlConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "UPDATE users SET name = ? WHERE id = ?")) {
+
+            preparedStatement.setString(1, newUsername);
+            preparedStatement.setInt(2, userIdToUpdate);
+
+            int rowsUpdated = preparedStatement.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("Data updated successfully!");
             }
+        }
+    }
 
+    public void updatePassword(int userIdToUpdate, String newPassword) throws SQLException {
+        try (Connection connection = MySqlConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "UPDATE users SET password = ? WHERE id = ?")) {
 
-        }catch (SQLException e){
-            e.printStackTrace();
+            preparedStatement.setString(1, newPassword);
+            preparedStatement.setInt(2, userIdToUpdate);
+
+            int rowsUpdated = preparedStatement.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("Password updated successfully!");
+            }
         }
     }
 }
